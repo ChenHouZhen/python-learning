9c6d674d38199748032d99796e92e8499c9ad0e3
chenhouzhen <chenhouzhen@yy.com>
2020-05-27 00:56:27 +0800

    opened window

--- spider/selenium/opera_opened_window.py
+++ spider/selenium/opera_opened_window.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+# 执行前需要进入 Chrome 的安装目录执行：chrome.exe --remote-debugging-port=9222
 
 URL_BAIDU = "https://www.baidu.com"
 
@@ -7,3 +8,4 @@ if __name__ == '__main__':
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
     driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get(URL_BAIDU)
